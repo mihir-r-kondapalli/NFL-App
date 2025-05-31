bd570f6b90bd8451ad6262a69e9728cd1ad2dc93
mihir-r-kondapalli <mihirkondapalli@ucsb.edu>
2025-05-31 14:34:20 -0700

    added vercel analytics

--- frontend/nfl-app/app/layout.tsx
+++ frontend/nfl-app/app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import './globals.css'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: '4th & Sim',
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+        </body>
     </html>
   )
 }
